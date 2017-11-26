aa245e331b2bbdf02d768b1252f570c633e295ae
Stephan Fuhrmann <s@sfuhrm.de>
2017-11-26 18:36:59 +0100

    Set version script

---
+++ scripts/set-version.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#
+# Sets the version of the current software at multiple places.
+# Call it like this:
+# set-version.sh 1.0.0-SNAPSHOT
+#
+# (c) 2017 Stephan Fuhrmann
+
+NEWVERSION=$1
+
+if [ "x${NEWVERSION}" = "x" ]; then
+	echo "Please give a version as a parameter, for example 0.1.4-SNAPSHOT"
+	exit 10
+fi
+
+ROOT=${PWD}
+
+echo "- pom.xml"
+mvn versions:set -DnewVersion=${NEWVERSION} || exit
+rm -f pom.xml.versionsBackup
+
