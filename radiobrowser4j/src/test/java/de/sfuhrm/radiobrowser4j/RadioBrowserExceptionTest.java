60b64246f6b786b7de9a57812c20233498937ba0
Stephan Fuhrmann <s@sfuhrm.de>
2019-04-29 20:57:27 +0200

    More tests

---
+++ radiobrowser4j/src/test/java/de/sfuhrm/radiobrowser4j/RadioBrowserExceptionTest.java
@@ -0,0 +1,43 @@
+/*
+* Copyright 2017 Stephan Fuhrmann
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package de.sfuhrm.radiobrowser4j;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test for the Exception.
+ * @author Stephan Fuhrmann
+ */
+public class RadioBrowserExceptionTest {
+
+    @Test
+    public void newWithMessage() {
+        RadioBrowserException exception = new RadioBrowserException("foo bar");
+        assertThat(exception.getMessage(), is("foo bar"));
+    }
+
+    @Test
+    public void newWithCause() {
+        RuntimeException runtime = new RuntimeException("bar");
+        RadioBrowserException exception = new RadioBrowserException(runtime);
+        assertThat(exception.getCause(), is(runtime));
+    }
+}
