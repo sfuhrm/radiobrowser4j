61af6e9c9eab1f54611867ade05a6ab91a7828d3
Stephan Fuhrmann <s@sfuhrm.de>
2019-04-29 20:09:45 +0200

    New test case

---
+++ radiobrowser4j/src/test/java/de/sfuhrm/radiobrowser4j/ListParameterTest.java
@@ -0,0 +1,83 @@
+/*
+* Copyright 2017 Stephan Fuhrmann
+*
+* Licensed under the Apache License, Version 2.0 (the "License");
+* you may not use this file except in compliance with the License.
+* You may obtain a copy of the License at
+*
+*     http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+package de.sfuhrm.radiobrowser4j;
+
+import org.junit.Test;
+
+import javax.ws.rs.core.MultivaluedHashMap;
+import javax.ws.rs.core.MultivaluedMap;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertThat;
+
+/**
+ * Test for the ListParameter class.
+ * @author Stephan Fuhrmann
+ */
+public class ListParameterTest {
+
+    @Test
+    public void create() {
+        ListParameter listParameter = ListParameter.create();
+    }
+
+    @Test
+    public void applyToWithNothingSet() {
+        ListParameter listParameter = ListParameter.create();
+        MultivaluedMap multivaluedMap = new MultivaluedHashMap();
+        listParameter.applyTo(multivaluedMap);
+
+        assertThat(multivaluedMap.keySet().toArray(), is(emptyArray()));
+    }
+
+    @Test
+    public void applyToWithOrderSet() {
+        ListParameter listParameter = ListParameter.create();
+        listParameter.order(FieldName.bitrate);
+        MultivaluedMap multivaluedMap = new MultivaluedHashMap();
+        listParameter.applyTo(multivaluedMap);
+
+        assertThat(multivaluedMap.keySet(),     is(new HashSet(Arrays.asList("order"))));
+        assertThat(multivaluedMap.get("order"),  is(Arrays.asList("bitrate")));
+    }
+
+    @Test
+    public void applyToWithReverseSet() {
+        ListParameter listParameter = ListParameter.create();
+        listParameter.reverseOrder(true);
+        MultivaluedMap multivaluedMap = new MultivaluedHashMap();
+        listParameter.applyTo(multivaluedMap);
+
+        assertThat(multivaluedMap.keySet(),     is(new HashSet(Arrays.asList("reverse"))));
+        assertThat(multivaluedMap.get("reverse"),  is(Arrays.asList("true")));
+    }
+
+    @Test
+    public void applyToWithOrderAndReverseSet() {
+        ListParameter listParameter = ListParameter.create();
+        listParameter.order(FieldName.codec);
+        listParameter.reverseOrder(true);
+        MultivaluedMap multivaluedMap = new MultivaluedHashMap();
+        listParameter.applyTo(multivaluedMap);
+
+        assertThat(multivaluedMap.keySet(),     is(new HashSet(Arrays.asList("reverse", "order"))));
+        assertThat(multivaluedMap.get("reverse"),  is(Arrays.asList("true")));
+        assertThat(multivaluedMap.get("order"),  is(Arrays.asList("codec")));
+    }
+}
